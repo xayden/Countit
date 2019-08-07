03a0d25dc36d3ea01d57d8c99ddfe081329ff6fa
Abdullah Osama <xaydenrosario@gmail.com>
2019-08-07 14:37:31 +0200

    Removing create-react-app junk; Adding notification SW

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as notification from './notification';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  requestNotificationPermission = () => {
+    notification.requestPermission();
+  };
+
+  render() {
+    return (
+      <div>
+        {notification.supportsNotification() && (
+          <button onClick={this.requestNotificationPermission}>Enable Notification</button>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/notification.js
@@ -0,0 +1,54 @@
+let supportLocalNotification = true,
+  serviceWorker = '',
+  permission = 'default';
+
+if (!('serviceWorker' in navigator) || !('PushManager' in window)) {
+  supportLocalNotification = false;
+}
+
+function registerServiceWorker() {
+  return navigator.serviceWorker
+    .register(`${process.env.PUBLIC_URL}/sw.js`)
+    .then(registeration => {
+      console.log('Service Worker successfully register!');
+      return registeration;
+    })
+    .catch(error => {
+      console.error('Unable to register service worker.', error);
+    });
+}
+
+function getUserPermission() {
+  return new Promise((resolve, reject) => {
+    const permissionResult = Notification.requestPermission(result => {
+      resolve(result);
+    });
+
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  }).then(async permissionResult => {
+    if (permissionResult !== 'granted') {
+      throw new Error('Permission denied.');
+    }
+  });
+}
+
+serviceWorker = registerServiceWorker();
+
+export function supportsNotification() {
+  return supportLocalNotification;
+}
+
+export async function requestPermission() {
+  permission = await getUserPermission();
+}
+
+export function getPermission() {
+  return permission;
+}
+
+export function sendNotification(title, body) {
+  const opts = { body };
+  serviceWorker.showNotification(title, opts);
+}
