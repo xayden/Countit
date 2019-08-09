63e40b397857b497be54483e76b80bc9830d6e98
Abdullah Osama <xaydenrosario@gmail.com>
2019-08-09 16:51:41 +0200

    Bug fix where time could not be changed for any timer
    This bug occured because of changing how the TimerWraper and Timer
    works, instead of nesting Timer inside of TimerWraper it is nested
    as props.children from the App component, this change in behaviour
    was not considered in changing time behaviour in Timer, which has
    introduced the bug.

--- src/Components/Rounds.js
+++ src/Components/Rounds.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function WastedTime(props) {
+export default function Rounds(props) {
   return (
     <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
-      <div class="rounded overflow-hidden mb-4 shadow-sm bg-danger h-150">
+      <div class="rounded overflow-hidden mb-4 shadow-sm bg-light h-150">
         <div class="col p-3 d-flex flex-column position-static text-center">
-          <h5 class="mt-n3">Wasted Time</h5>
-          <h3 class="mt-4">00:00:00</h3>
+          <h5 class="mt-n3">Rounds</h5>
+          <h3 class="mt-4">1</h3>
         </div>
       </div>
     </div>
--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -4,25 +4,58 @@ export default class Timer extends Component {
   constructor(props) {
     super(props);
 
-    let seconds = Math.floor((this.props.time / 1000) % 60),
-      minutes = Math.floor((this.props.time / (1000 * 60)) % 60),
-      hours = Math.floor((this.props.time / (1000 * 60 * 60)) % 24);
-
-    hours = hours < 10 ? '0' + hours : hours;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+    const { hours, minutes, seconds } = this.humanReadableTime(this.props.time);
 
     this.state = {
       hours,
       minutes,
       seconds,
+      currentTime: this.props.time,
       editing: ''
     };
   }
 
+  start = () => {
+    this.timer = setInterval(() => {
+      this.setState(prevState => {
+        if (prevState.currentTime <= 0) {
+          this.props.onTimerFinish(); // react throws an error in the console for this!
+          this.setState({ currentTime: this.props.time });
+          return this.pause();
+        } else {
+          return { currentTime: prevState.currentTime - 1000 };
+        }
+      });
+    }, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.timer);
+    delete this.timer;
+    this.props.resumeWastedTimer();
+  };
+
+  resume = () => {
+    if (!this.timer) this.start();
+    this.props.pauseWastedTimer();
+  };
+
+  humanReadableTime = ms => {
+    let seconds = Math.floor((ms / 1000) % 60),
+      minutes = Math.floor((ms / (1000 * 60)) % 60),
+      hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    return { hours, minutes, seconds };
+  };
+
   handleChange = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    if (value.length < 3 && (Number(value) || value === '')) {
+      this.setState({ [name]: value });
+    }
   };
 
   handleTimeEdit = name => {
@@ -31,17 +64,41 @@ export default class Timer extends Component {
     });
   };
 
-  componentDidUpdate(preProps, prevState) {
+  handleSaveEdit = () => {
+    const ms = this.state.seconds * 1000 + this.state.minutes * 60 * 1000 + this.state.hours * 60 * 60 * 1000;
+    const { hours, minutes, seconds } = this.humanReadableTime(ms);
+
+    this.setState({ currentTime: ms, editing: '', hours, minutes, seconds });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
     if (!prevState.editing && this.state.editing) {
       const node = document.getElementById(this.state.editing);
       node.focus();
       node.select();
+    } else if (
+      prevProps.currentPlayingTimer !== this.props._id &&
+      this.props.currentPlayingTimer === this.props._id
+    ) {
+      this.resume();
+    } else if (
+      prevProps.currentPlayingTimer === this.props._id &&
+      this.props.currentPlayingTimer !== this.props._id
+    ) {
+      this.pause();
     }
   }
 
   render() {
+    const { hours, minutes, seconds } = this.humanReadableTime(this.state.currentTime);
     return (
-      <h3 className="align-self-center">
+      <h3
+        className={
+          this.props.currentPlayingTimer === this.props._id
+            ? 'align-self-center text-warning'
+            : 'align-self-center'
+        }
+      >
         {this.state.editing === 'hours' ? (
           <input
             id="hours"
@@ -51,10 +108,10 @@ export default class Timer extends Component {
             value={this.state.hours}
             onChange={this.handleChange}
             onKeyDown={this.handleKeyDown}
-            onBlur={() => this.setState({ editing: '' })}
+            onBlur={this.handleSaveEdit}
           />
         ) : (
-          <span onClick={() => this.handleTimeEdit('hours')}>{this.state.hours}</span>
+          <span onClick={() => this.handleTimeEdit('hours')}>{hours}</span>
         )}
         :
         {this.state.editing === 'minutes' ? (
@@ -66,10 +123,10 @@ export default class Timer extends Component {
             value={this.state.minutes}
             onChange={this.handleChange}
             onKeyDown={this.handleKeyDown}
-            onBlur={() => this.setState({ editing: '' })}
+            onBlur={this.handleSaveEdit}
           />
         ) : (
-          <span onClick={() => this.handleTimeEdit('minutes')}>{this.state.minutes}</span>
+          <span onClick={() => this.handleTimeEdit('minutes')}>{minutes}</span>
         )}
         :
         {this.state.editing === 'seconds' ? (
@@ -81,10 +138,10 @@ export default class Timer extends Component {
             value={this.state.seconds}
             onChange={this.handleChange}
             onKeyDown={this.handleKeyDown}
-            onBlur={() => this.setState({ editing: '' })}
+            onBlur={this.handleSaveEdit}
           />
         ) : (
-          <span onClick={() => this.handleTimeEdit('seconds')}>{this.state.seconds}</span>
+          <span onClick={() => this.handleTimeEdit('seconds')}>{seconds}</span>
         )}
       </h3>
     );
